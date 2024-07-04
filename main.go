0bb5d680be07af2f44aa172e80cab0b0d0abfbdf
DevMiner <devminer@devminer.xyz>
2024-07-04 12:16:24 +0200

    chore: clean up

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math"
 	"math/big"
 	"os"
@@ -43,9 +42,7 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to download Node 20 image")
 	}
 
-	packageName, err := runPrompt(&promptui.Prompt{
-		Label: "Package",
-	})
+	packageName, err := runPrompt(&promptui.Prompt{Label: "Package"})
 	if err != nil {
 		log.Fatal().Err(err).Msg("Prompt failed")
 	}
@@ -57,39 +54,17 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to fetch package info")
 	}
 
-	log.Debug().Msgf("Fetched package info for %s", packageInfo.Name)
-
 	downloads, err := npmClient.GetPackageDownloadsLastWeek(packageName)
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to fetch package downloads")
 	}
 
-	log.Debug().Msgf("Fetched download counts for %s", packageInfo.Name)
+	log.Debug().Msgf("Fetched package info for %s", packageInfo.Name)
 
-	versions := make([]npm_version.Version, 0)
-	for _, v := range packageInfo.Versions {
-		versions = append(versions, v.Version)
-	}
-
-	sort.Sort(npm_version.Collection(versions))
-	slices.Reverse(versions)
-
-	_, packageVersion, err := runSelect(&promptui.Select{
-		Label: "Select version",
-		Items: versions,
-		Size:  int(math.Min(float64(len(versions)), 16)),
-		Searcher: func(input string, index int) bool {
-			return strings.Contains(versions[index].String(), input)
-		},
-	})
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to select version")
-	}
-
+	packageVersion := promptPackageVersion(packageInfo)
 	packageJson := packageInfo.Versions[packageVersion]
-	log.Info().Str("version", packageJson.JSON.Version).Msg("Selected version")
-
 	downloadsLastWeek := downloads[packageJson.JSON.Version]
+	log.Info().Str("version", packageJson.JSON.Version).Msg("Selected version")
 
 	oldPackageSize, tmpDir, err := measurePackageSize(dockerC, packageInfo.LatestVersion.JSON.AsDependency())
 	if err != nil {
@@ -103,142 +78,81 @@ func main() {
 		log.Fatal().Err(err).Msg("Failed to parse package-lock.json")
 	}
 
-	dependencies := make([]npm.DependencyInfo, 0, len(packageJson.JSON.Dependencies)+1)
-	for _, k := range packageJson.JSON.Dependencies {
-		dep, ok := pkgLock.Packages[k.Name]
-		if !ok {
-			log.Warn().Str("dependency", k.Name).Msg("Dependency not found")
-			continue
-		}
-
-		dependencies = append(dependencies, dep.AsDependency())
-	}
-
-	removedDependencies, err := ui_components.NewMultiSelect("Removed dependencies", dependencies).Run()
-	if err != nil {
-		log.Fatal().Err(err).Msg("Failed to run multi select")
-	}
-
-	for _, d := range removedDependencies {
-		log.Info().Msgf("Marked dependency as removed: %s", d.AsNPMString())
-	}
+	removedDependencies := promptRemovedDependencies(&packageJson.JSON, pkgLock)
 
 	addedDependencies, err := ui_components.NewEditableList("Added dependencies", resolveNPMPackage(npmClient)).Run()
 	if err != nil {
 		log.Fatal().Err(err).Msg("Failed to run editable list")
 	}
 
-	for _, d := range addedDependencies {
-		log.Info().Msgf("Marked dependency as added: %s", d.AsDependency().AsNPMString())
-	}
+	deps := combineDependencies(removedDependencies, addedDependencies)
+	for depName, dep := range deps {
+		l := log.With().Str("package", depName).Logger()
 
-	type dependencyPackageInfo struct {
-		Dependency        npm.DependencyInfo
-		Size              uint64
-		DownloadsLastWeek uint64
-	}
-	removedPackageSizes := map[string]dependencyPackageInfo{}
-	for _, p := range removedDependencies {
-		l := log.With().Str("package", p.AsNPMString()).Logger()
-
-		downloadsLastWeek := uint64(0)
-		{
-			downloads, err := npmClient.GetPackageDownloadsLastWeek(p.Name)
-			if err != nil {
-				l.Error().Err(err).Msg("Failed to fetch package downloads")
-			} else {
-				downloadsLastWeek = downloads[p.Version]
-				l.Info().Msgf("Downloads last week: %v", downloadsLastWeek)
-			}
+		downloads, err := npmClient.GetPackageDownloadsLastWeek(dep.Name)
+		if err != nil {
+			l.Error().Err(err).Msg("Failed to fetch package downloads")
+		} else {
+			dep.DownloadsLastWeek = downloads[dep.Version]
+			l.Info().Msgf("Downloads last week: %v", downloadsLastWeek)
 		}
 
-		packageSize, _, err := measurePackageSize(dockerC, p)
+		dep.Size, _, err = measurePackageSize(dockerC, dep.DependencyInfo)
 		if err != nil {
 			l.Fatal().Err(err).Msg("Failed to measure package size")
 		}
 
-		l.Info().Str("size", humanize.Bytes(packageSize)).Msg("Package size")
-
-		removedPackageSizes[p.AsNPMString()] = dependencyPackageInfo{
-			Dependency:        p,
-			Size:              packageSize,
-			DownloadsLastWeek: downloadsLastWeek,
-		}
+		l.Info().Msgf("Package size: %s", humanize.Bytes(dep.Size))
 	}
 
-	addedPackageSizes := map[string]dependencyPackageInfo{}
-	for _, p := range addedDependencies {
-		l := log.With().Str("package", p.AsDependency().AsNPMString()).Logger()
-
-		downloadsLastWeek := uint64(0)
-		{
-			downloads, err := npmClient.GetPackageDownloadsLastWeek(p.Name)
-			if err != nil {
-				l.Error().Err(err).Msg("Failed to fetch package downloads")
-			} else {
-				downloadsLastWeek = downloads[p.Version]
-			}
-		}
-
-		packageSize, _, err := measurePackageSize(dockerC, p.AsDependency())
-		if err != nil {
-			l.Fatal().Err(err).Msg("Failed to measure package size")
-		}
-
-		l.Info().Str("size", humanize.Bytes(packageSize)).Msg("Package size")
-
-		addedPackageSizes[p.AsDependency().AsNPMString()] = dependencyPackageInfo{
-			Dependency:        p.AsDependency(),
-			Size:              packageSize,
-			DownloadsLastWeek: downloadsLastWeek,
-		}
-	}
-
-	fmt.Println()
+	log.Info().Send()
 	log.Info().Msgf("Package size report")
 	log.Info().Msgf("===================")
-
+	log.Info().Send()
 	log.Info().Msgf("Package info for %s: %s", packageInfo.Name, humanize.Bytes(oldPackageSize))
 	log.Info().Msgf("  Latest version: %s", packageInfo.LatestVersion.Version)
 	log.Info().Msgf("  Last released: %s (%s ago)", packageInfo.LatestVersion.ReleaseTime, time_helpers.FormatDuration(time.Since(packageInfo.LatestVersion.ReleaseTime)))
-	log.Info().Msgf("  Downloads last week of %s: %s", packageInfo.LatestVersion.Version, humanize.FormatInteger("#,###.", int(downloadsLastWeek)))
+	log.Info().Msgf("  Downloads last week of %s: %s", packageInfo.LatestVersion.Version, fmtInt(int(downloadsLastWeek)))
 	log.Info().Msgf("  Estimated traffic last week: %s", humanize.Bytes(downloadsLastWeek*oldPackageSize))
 
 	if len(removedDependencies) > 0 {
 		log.Info().Msg("Removed dependencies:")
 		for _, p := range removedDependencies {
-			info := removedPackageSizes[p.AsNPMString()]
+			info := deps[p.AsNPMString()]
+			traffic := info.DownloadsLastWeek * info.Size
 			pcTraffic := float64(downloadsLastWeek) * 100 / float64(info.DownloadsLastWeek)
 
 			log.Info().Msgf("  %s@%s: %s", p.Name, p.Version, humanize.Bytes(info.Size))
-			log.Info().Msgf("    Downloads last week: %s", humanize.FormatInteger("#,###.", int(info.DownloadsLastWeek)))
-			log.Info().Msgf("    Estimated traffic last week: %s", humanize.Bytes(info.DownloadsLastWeek*info.Size))
-			log.Info().Msgf("    Estimated %% of traffic because of \"%s\": %s%%", packageInfo.LatestVersion.JSON.AsDependency().AsNPMString(), humanize.FormatFloat("#,###.##", pcTraffic))
+			log.Info().Msgf("    Downloads last week: %s (%s)", fmtInt(int(info.DownloadsLastWeek)), fmtInt(int(downloadsLastWeek)))
+			log.Info().Msgf("    Estimated traffic last week: %s (%s coming from \"%s\")", humanize.Bytes(traffic), humanize.Bytes(traffic*uint64(pcTraffic)/100), packageJson.JSON.AsDependency().AsNPMString())
+			log.Info().Msgf("    Estimated %% of traffic because of \"%s\": %s%%", packageJson.Version, fmtPercent(pcTraffic))
 		}
 	}
 
 	if len(addedDependencies) > 0 {
 		log.Info().Msg("Added dependencies:")
 		for _, p := range addedDependencies {
-			info := addedPackageSizes[p.AsDependency().AsNPMString()]
+			info := deps[p.AsDependency().AsNPMString()]
+
 			log.Info().Msgf("  %s@%s: %s", p.Name, p.Version, humanize.Bytes(info.Size))
-			log.Info().Msgf("    Latest version: %s", info.Dependency.Version)
-			log.Info().Msgf("    Downloads last week: %s (+%s)", humanize.FormatInteger("#,###.", int(info.DownloadsLastWeek)), humanize.FormatInteger("#,###.", int(downloadsLastWeek)))
+			log.Info().Msgf("    Latest version: %s", info.Version)
+			log.Info().Msgf("    Downloads last week: %s (+%s)", fmtInt(int(info.DownloadsLastWeek)), fmtInt(int(downloadsLastWeek)))
 			log.Info().Msgf("    Estimated traffic last week: %s", humanize.Bytes(info.DownloadsLastWeek*info.Size))
 		}
 	}
 
 	newTotalSize := oldPackageSize
-	for _, p := range removedPackageSizes {
-		newTotalSize -= p.Size
-	}
-	for _, p := range addedPackageSizes {
-		newTotalSize += p.Size
+	for _, p := range deps {
+		if p.Type == DependencyRemoved {
+			newTotalSize -= p.Size
+		} else {
+			newTotalSize += p.Size
+		}
 	}
 
 	pcSize := 100 * float64(newTotalSize) / float64(oldPackageSize)
 	sizeChange := 100 - float64(oldPackageSize)*100/float64(newTotalSize)
-	pcChangeFmt := humanize.FormatFloat("#,###.##", sizeChange)
+	pcChangeFmt := fmtPercent(sizeChange)
 	if sizeChange == 0 {
 		pcChangeFmt = " " + pcChangeFmt
 	} else if sizeChange > 0 {
@@ -263,7 +177,7 @@ func main() {
 			"Total size change: %s -> %s (%s%%, %s%%)",
 			humanize.Bytes(oldPackageSize),
 			humanize.Bytes(newTotalSize),
-			humanize.FormatFloat("#,###.##", pcSize),
+			fmtPercent(pcSize),
 			pcChangeFmt,
 		)
 	log.Info().
@@ -328,4 +242,94 @@ func resolveNPMPackage(client *npm.Client) ui_components.StringToItemConvertFunc
 
 		return nil, ui_components.ErrRetry
 	}
+}
+
+func fmtPercent(v float64) string {
+	return humanize.FormatFloat("#,###.##", v)
+}
+
+func fmtInt(v int) string {
+	return humanize.FormatInteger("#,###.", v)
+}
+
+type dependencyPackageInfoType uint8
+
+const (
+	DependencyRemoved dependencyPackageInfoType = iota
+	DependencyAdded
+)
+
+type dependencyPackageInfo struct {
+	npm.DependencyInfo
+	Size              uint64
+	DownloadsLastWeek uint64
+	Type              dependencyPackageInfoType
+}
+
+func combineDependencies(removedDependencies []npm.DependencyInfo, addedDependencies []*npm.PackageJSON) map[string]*dependencyPackageInfo {
+	deps := map[string]*dependencyPackageInfo{}
+
+	for _, d := range removedDependencies {
+		deps[d.AsNPMString()] = &dependencyPackageInfo{
+			DependencyInfo:    d,
+			Type:              DependencyRemoved,
+			Size:              0,
+			DownloadsLastWeek: 0,
+		}
+	}
+
+	for _, d := range addedDependencies {
+		deps[d.AsDependency().AsNPMString()] = &dependencyPackageInfo{
+			DependencyInfo:    d.AsDependency(),
+			Type:              DependencyAdded,
+			Size:              0,
+			DownloadsLastWeek: 0,
+		}
+	}
+
+	return deps
+}
+
+func promptRemovedDependencies(packageJson *npm.PackageJSON, pkgLock *npm.PackageLockJSON) []npm.DependencyInfo {
+	dependencies := make([]npm.DependencyInfo, 0, len(packageJson.Dependencies))
+	for _, k := range packageJson.Dependencies {
+		dep, ok := pkgLock.Packages[k.Name]
+		if !ok {
+			log.Warn().Str("dependency", k.Name).Msg("Dependency not found")
+			continue
+		}
+
+		dependencies = append(dependencies, dep.AsDependency())
+	}
+
+	removedDependencies, err := ui_components.NewMultiSelect("Removed dependencies", dependencies).Run()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to run multi select")
+	}
+
+	return removedDependencies
+}
+
+func promptPackageVersion(packageInfo *npm.PackageInfo) string {
+	versions := make([]npm_version.Version, 0)
+	for _, v := range packageInfo.Versions {
+		versions = append(versions, v.Version)
+	}
+
+	sort.Sort(npm_version.Collection(versions))
+	slices.Reverse(versions)
+
+	_, packageVersion, err := runSelect(&promptui.Select{
+		Label: "Select version",
+		Items: versions,
+		Size:  int(math.Min(float64(len(versions)), 16)),
+		Searcher: func(input string, index int) bool {
+			return strings.Contains(versions[index].String(), input)
+		},
+	})
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to select version")
+	}
+
+	return packageVersion
 }
