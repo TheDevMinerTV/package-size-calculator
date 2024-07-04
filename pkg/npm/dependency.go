4cf367d4263642d0ffded1f27545de5934ca7fd7
DevMiner <devminer@devminer.xyz>
2024-07-04 12:17:44 +0200

    chore: remove devDependencies

--- pkg/npm/dependency.go
+++ pkg/npm/dependency.go
@@ -2,25 +2,13 @@ package npm
 
 import "fmt"
 
-type DependencyType string
-
-const (
-	DependencyTypeNormal DependencyType = "normal"
-	DependencyTypeDev    DependencyType = "dev"
-)
-
-func (d DependencyType) String() string {
-	return string(d)
-}
-
 type DependencyInfo struct {
 	Name    string
 	Version string
-	Type    DependencyType
 }
 
 func (d DependencyInfo) String() string {
-	return fmt.Sprintf("%s @ %s (%s)", d.Name, d.Version, d.Type)
+	return fmt.Sprintf("%s @ %s", d.Name, d.Version)
 }
 
 func (d DependencyInfo) AsNPMString() string {
